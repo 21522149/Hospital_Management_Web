cd65f47ed090691641ec84e9045f73fa53bb58a1
Nguyễn Đặng Thanh Huy <102576374+21522149@users.noreply.github.com>
2024-02-01 22:03:00 +0700

    Khởi tạo dự án

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        blue: "#0067FF",
+        yellow: "#FEB60D",
+        purple: "#9771FF",
+        headingColor: "#181A1E",
+        textColor:"#4E545F"
+      },
+      
+      boxShadow: {
+        panelShadow: "rgba(17,12,46,0.15) 0px 48px 100px 0px;",
+      },
+    },
+  },
+  plugins: [],
+}
+
