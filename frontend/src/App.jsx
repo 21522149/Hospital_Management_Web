3079d7c84a14dbfaa5e588dba119d596cc432d65
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2024-02-06 23:35:49 +0700

    [change + add]: images, struture of file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
+import Home from "./page/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <Header/>
-      <Home/>
+      <Header />
+      <div>
+        <Home/>
+        <div>
+          <Footer/>
+        </div>
+      </div>
     </div>
   );
 }
