cb7ab55400870d52a296e676655f3b77e32e979f
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2024-02-19 15:29:35 +0700

    feat(login): set router

--- frontend/src/page/login/Login.jsx
+++ frontend/src/page/login/Login.jsx
@@ -4,30 +4,30 @@ import React from "react";
 const Login = () => {
     return (
         <div>
-            
+
             <div>
                 <h1>Login</h1>
-                
-               <form action="">
-                   
-                   <div>
-                       <input type="email" />
-                       <label htmlFor="">Your Email</label>
-                   </div>
 
-                   <div>
-                       <input type="password" />
-                       <label htmlFor="">Your Password</label>
-                   </div>
+                <form action="">
 
-                   <div>
-                       <input type="checkbox" name="" />
-                       <label htmlFor="Remember Me"></label>
-                       <span>Forgot Password?</span>
+                    <div>
+                        <input type="email" />
+                        <label htmlFor="">Your Email</label>
                     </div>
-                   
-                   <button type="submit">Login</button>
-               </form>
+
+                    <div>
+                        <input type="password" />
+                        <label htmlFor="">Your Password</label>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" name="" />
+                        <label htmlFor="Remember Me"></label>
+                        <span>Forgot Password?</span>
+                    </div>
+
+                    <button type="submit">Login</button>
+                </form>
 
             </div>
 
